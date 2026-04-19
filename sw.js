1757d9d361cf0438336f6a96b223acd6b761838f
abdodaim14-ai <abdodaim14@gmail.com>
2026-04-19 08:01:39 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+const cacheName = 'maki-quran-v1';
+const assets = ['./', './index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
